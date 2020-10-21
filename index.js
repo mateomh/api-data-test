4bc601fe22c96dd8ebdd4478d8ab20d06fd823d4
mateomh <mmojicah.84@gmail.com>
2020-10-21 16:39:16 -0500

    Create game with code with fetch

---
+++ index.js
@@ -0,0 +1,21 @@
+// Game creation run just once
+// let apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+// const game = {
+//   "name": "Mm The Game"
+// };
+// gameId = "VweAZKDll3qd5Cc5zJGw"; made with postman
+// const fetchOptions = {
+//   headers: {
+//     "Content-Type": "application/json; charset=UTF-8"
+//   },
+//   body: JSON.stringify(game),
+//   method: "POST",
+// };
+
+// fetch(apiUrl, fetchOptions)
+// .then( data =>{ return data.json(); }) // converts the response into json
+// .then( res => { console.log(res); }) // prints the data to the console
+// .catch( error => { console.log(error) }); // if there is an error it logs it to the console
+
+const gameID='uBQGIE809ExVe6lR1BYH'; // made with the code above
+
