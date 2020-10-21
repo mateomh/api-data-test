bff044b72a570a1c21d0421361be0b597adacd16
mateomh <mmojicah.84@gmail.com>
2020-10-21 17:45:07 -0500

    Add score functionality

--- index.js
+++ index.js
@@ -16,6 +16,43 @@
 // .then( data =>{ return data.json(); }) // converts the response into json
 // .then( res => { console.log(res); }) // prints the data to the console
 // .catch( error => { console.log(error) }); // if there is an error it logs it to the console
-
 const gameID='uBQGIE809ExVe6lR1BYH'; // made with the code above
+let apiScoreURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+apiScoreURL = apiScoreURL + gameID + '/scores/';
 
+
+function updateMessage(data) {
+  const msg = document.getElementById('message');
+  console.log(data);
+  //msg.innerHTML = resp.result;
+}
+
+function addScore() { 
+
+  const player = document.getElementById('player-name');
+  const score = document.getElementById('player-score');
+
+  const scoreData = {
+    "user": player.nodeValue,
+    "score": score.value,
+  };
+
+  const fetchOptions = {
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8"
+    },
+    body: JSON.stringify(scoreData),
+    method: "POST",
+  };
+
+  const resp = fetch(apiScoreURL, fetchOptions)
+  .then( data => { return data.json() } ) // converts the response into json
+  .then( response => {updateMessage(response)})
+  .catch( error => { console.log(error) }); // if there is an error it logs it to the console
+
+  
+}
+
+const scrbtn = document.getElementById('score-button');
+scrbtn.addEventListener('click', addScore);
+//console.log(apiScoreURL);
